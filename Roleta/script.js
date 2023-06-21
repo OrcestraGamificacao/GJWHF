0fbdd92fbda05268311274a0336d01550d44064a
Maria Alice <marialice3003@gmail.com>
2023-06-20 22:53:58 -0300

    feat: Atualização nos temas das roletas, angulações corrigidas e botão central pulsante

--- Roleta/script.js
+++ Roleta/script.js
@@ -3,6 +3,7 @@
 //cada usuário responde apenas 5 perguntas do banco
 
 
+//Iniciar o giro da roleta
 function playOnClick() {   
     globalObjects = {
         btnPlay: document.getElementById("btnPlay"),
@@ -16,6 +17,7 @@ function playOnClick() {
     globalObjects.roleta.style.animation = "roleta 1s linear infinite";
 }
 
+//Calcular qual será o tema sorteado
 function calculate() {
     var timeFinal = new Date();
     var tempo = Math.abs(timeFinal - globalObjects.timeInitial);
@@ -27,10 +29,11 @@ function calculate() {
     return box;
 }
 
+//Parar o giro da roleta e exibir o tema sorteado
 function stopOnClick() {
     globalObjects.roleta.style["animation-play-state"] = "paused";
     // globalObjects.btnStop.style.visibility = "hidden";
     var box = calculate();
     var boxGanhador = document.getElementById("opt".concat(box))
-    document.getElementById("msgGanhador").innerHTML = "Parabéns! Você ganhou ".concat(boxGanhador.innerHTML);
+    document.getElementById("msgGanhador").innerHTML = "O tema sorteado foi:  ".concat(boxGanhador.innerHTML);
 }
