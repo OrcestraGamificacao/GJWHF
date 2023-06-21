033775244a3d9dee4b7791e1a0d21d7bcfd58553
anabborges <abarbosaborges17@gmail.com>
2023-06-21 16:47:14 -0300

    adicionando alternativaD na classe Questao

--- Model/modelQuestao.js
+++ Model/modelQuestao.js
@@ -10,11 +10,12 @@
 // }
 
 //
-function Questao(numQuestao, pergunta, alternativaA, alternativaB, alternativaC, resposta){
+function Questao(numQuestao, pergunta, alternativaA, alternativaB, alternativaC, alternativaD, resposta){
     this.numQuestao = numQuestao
     this.pergunta = pergunta
     this.alternativaA = alternativaA
     this.alternativaB = alternativaB
     this.alternativaC = alternativaC
+    this.alternativaD = alternativaD
     this.resposta = resposta
 }
