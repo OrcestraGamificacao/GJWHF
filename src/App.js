5715dc3fad4fc13e2ef1994b16cd4cff61262774
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-06-22 11:37:32 -0300

    React Project - Telas da Glória

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="background">
+        <div className="rodape"></div>
+      </div>
+      <div className="back-text-apresentation">
+          <h3>Olá, eu sou a Gloria, uma inteligência artificial, uma robozinha, focada em acabar com a violência contra mulheres e meninas!</h3>
+          <h4>Oferecimento</h4>
+          <h5>Orc'estra Gamificação | Instituto Glória | Na Praia</h5>
+          <div className="model-3d"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Napraia_background.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Napraia_background() {
+  return (
+    <div>Napraia_background</div>
+  )
+}
+
+export default Napraia_background
