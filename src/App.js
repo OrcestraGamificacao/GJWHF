58030f2224790d049d6bb8465d698be75f6369ef
Camila Careli <camilacsouzza@gmail.com>
2023-06-23 11:52:46 -0300

    +2 telas intradutorias

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./styles/App.css";
 import Box_Apresentacao from "./components/Box_Apresentacao";
+//import Box_Apresentacao2 from "./components/Box_Apresentacao2";
+//import Box_Apresentacao3 from "./components/Box_Apresentacao3";
 
 function App() {
   return (
     <div className="App">
       <div className="background">
         <Box_Apresentacao />
+        {/*<Box_Apresentacao2 />*/}
+        {/*<Box_Apresentacao3 />*/}
       </div>
       
     </div>
