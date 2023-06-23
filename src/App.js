1d12969778475100a227994f9011a37fba1d391b
Maria Alice <marialice3003@gmail.com>
2023-06-23 11:14:00 -0300

    feat: Passando os botões de html pra react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 import Box_Apresentacao from "./components/Box_Apresentacao";
+import PerguntaOpcoes from "./pages/PerguntaOpcoes"
 
 function App() {
   return (
     <div className="App">
       <div className="background">
-        <Box_Apresentacao />
+        <Box_Apresentacao/>
+        {/* <PerguntaOpcoes/> */}
       </div>
       
     </div>
