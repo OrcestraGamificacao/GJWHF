5f23e8503d0b74992e98670cbd4e2231e23a555e
Amanda De Moura <amandstudy@gmail.com>
2023-06-24 16:10:43 -0300

    aparecer as logos, sem responsiviadde(comented)

--- src/components/BoxApresentacao.jsx
+++ src/components/BoxApresentacao.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import "../styles/BoxApresentacao.css";
 
+/*
+  Jeito para aparecer as logos, mas sem responsiviadde ainda
+
+import OrcNew from './assets/OrcNew.png';
+import GloriaNew from  './assets/GloriaNew.png';
+import NaPraiaNew from './assets/NaPraiaNew.png';
+
+*/ 
+
 function BoxApresentacao() {
   return (
     <div>
@@ -10,9 +19,9 @@ function BoxApresentacao() {
           <h3 className="box-apresentation-title">
             Olá, eu sou a Glória, uma inteligência artificial com o propósito de combater a violência contra mulheres e meninas. Sou uma 'robozinha' desenvolvida com a missão de contribuir para um mundo mais seguro e igualitário para todas as mulheres. Meu objetivo principal é criar conscientização, educar e fornecer recursos para prevenir a violência e apoiar as vítimas. Para me ajudar nessa missão, tenho o oferecimento de:
             <div className="logos-container">
-              <img src="src\components\assets\OrcNew.png" alt="Logo Orc" />
-              <img src="src\components\assets\GloriaNew.png" alt="Logo Gloria" />
-              <img src="src\components\assets\NaPraiaNew.png" alt="Logo Na Praia" />
+              <img /*src={OrcNew}*/ src="src\components\assets\OrcNew.png" alt="Logo Orc" />
+              <img /*src={GloriaNew}*/ src="src\components\assets\GloriaNew.png" alt="Logo Gloria" />
+              <img /*src={NaPraiaNew}*/ src="src\components\assets\NaPraiaNew.png" alt="Logo Na Praia" />
             </div>
           </h3>
         </div>
