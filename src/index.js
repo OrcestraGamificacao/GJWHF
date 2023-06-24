4011abccb6db76b3a67f378dd6ce38bbefd45863
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-06-23 23:49:13 -0300

    Update index.js
    Adicionando as rotas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import Invite from  './pages/Invite';
 import App from './App';
+
+// 1 - configurando o router
+import{
+  createBrowserRouter, RouterProvider
+} from "react-router-dom";
+
+import PerguntaComentario from './pages/PerguntaComentario';
+import PerguntaFullText from './pages/PerguntaFullText';
+import PerguntaOpcoes from './pages/PerguntaOpcoes';
+import PremioBomDesempenho from './pages/PremioBomDesempenho';
+import PremioParticipacao from './pages/PremioParticipacao';
+import Resposta from './pages/Resposta';
+import TelaRoleta from './pages/TelaRoleta';
+import TemaEscolhido from './pages/TemaEscolhido';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/Invite',
+    element: <Invite />
+  },
+  {
+    path: '/PerguntaComentario',
+    element: <PerguntaComentario />
+  },
+  {
+    path: '/PerguntaFullText',
+    element: <PerguntaFullText />
+  },
+  {
+    path: '/PerguntaOpcoes',
+    element: <PerguntaOpcoes />
+  },
+  {
+    path: '/PremioBomDesempenho',
+    element: <PremioBomDesempenho />
+  },
+  {
+    path: '/PremioParticipacao',
+    element: <PremioParticipacao />
+  },
+  {
+    path: '/Resposta',
+    element: <Resposta />
+  },
+  {
+    path: 'TelaRoleta',
+    element: <TelaRoleta />
+  },
+  {
+    path: '/TemaEscolhido',
+    element: <TemaEscolhido />
+  },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
