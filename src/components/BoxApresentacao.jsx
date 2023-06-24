13b63ac60cac86bb56d5a9fc4c27776e6ba95550
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-06-23 22:43:34 -0300

    Atualizando para chamar as novas logos

--- src/components/BoxApresentacao.jsx
+++ src/components/BoxApresentacao.jsx
@@ -9,9 +9,9 @@ function BoxApresentacao() {
           <h3 className="box-apresentation-title">
             Olá, eu sou a Glória, uma inteligência artificial com o propósito de combater a violência contra mulheres e meninas. Sou uma 'robozinha' desenvolvida com a missão de contribuir para um mundo mais seguro e igualitário para todas as mulheres. Meu objetivo principal é criar conscientização, educar e fornecer recursos para prevenir a violência e apoiar as vítimas. Para me ajudar nessa missão, tenho o oferecimento de:
             <div className="logos-container">
-              <img src="src\components\assets\LogoOrc.png" alt="Logo Orc" />
-              <img src="src\components\assets\LogoGloria.png" alt="Logo Gloria" />
-              <img src="src\components\assets\LogoNaParia.png" alt="Logo Na Praia" />
+              <img src="src\components\assets\OrcNew.png" alt="Logo Orc" />
+              <img src="src\components\assets\GloriaNew.png" alt="Logo Gloria" />
+              <img src="src\components\assets\NaPraiaNew.png" alt="Logo Na Praia" />
             </div>
           </h3>
         </div>
