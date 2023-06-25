b7aa04ff52022c3635c5dba40b5eb19cb2669b6c
Amanda De Moura <amandstudy@gmail.com>
2023-06-25 16:59:32 -0300

    Tela PerguntaFull

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import "./styles/App.css";
 import Gloria from "./components/Gloria";
 import BoxApresentacao from "./components/BoxApresentacao";
 import BotaoAvancar from "./components/BotaoAvancar";
-import Roleta from "./components/Roleta/Roleta"
-import PerguntaOpcoes from "./pages/PerguntaOpcoes";
-import TelaRoleta from "./pages/Tela Roleta/TelaRoleta"
 
 function App() {
   return (
     <div className="App">
       <div className="background">
-        {/* <BoxApresentacao/> */}
-        {/* <Gloria/> */}
-        {/* <BotaoAvancar /> */}
-        {/* <PerguntaOpcoes/> */}
-        <TelaRoleta/>
+        <BoxApresentacao/>
+        <Gloria/>
+        <BotaoAvancar />
       </div>
 
     </div>
---
+++ src/components/BoxPerguntaFull.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/BoxPerguntaFull.css";
+
+const BoxPerguntaFull = () => {
+  return (
+    <div>
+      <div className="box-perguntaFull"></div>
+    </div>
+  );
+};
+
+export default BoxPerguntaFull;
