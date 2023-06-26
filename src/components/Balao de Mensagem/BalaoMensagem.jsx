822f900287b784eae27d46f666c5163421d813df
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-06-26 02:38:09 -0300

    Update BalaoMensagem.jsx

--- src/components/Balao de Mensagem/BalaoMensagem.jsx
+++ src/components/Balao de Mensagem/BalaoMensagem.jsx
@@ -4,7 +4,7 @@ import "./BalaoMensagem.css"
 const BalaoMensagem = (props) => {
     return (
         <div className="frame">
-            <button className="custom-btn btn-1">
+            <button className="custom-btn-btn-1">
             <span>{props.texto}
                 {/* Olá, eu sou a Glória! <br />
                 Vou começar explicando a gamificação! */}
