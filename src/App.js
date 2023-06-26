a98182e566a50062912bf4affd5199e18bec58f2
Camila Careli <camilacsouzza@gmail.com>
2023-06-26 00:49:20 -0300

    descomentei a roleta p/ aparecer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './styles/App.css'
-import Gloria from './components/Gloria'
-import BoxApresentacao from './components/BoxApresentacao'
-import BotaoAvancar from './components/BotaoAvancar'
+//import Gloria from './components/Gloria'
+//import BoxApresentacao from './components/BoxApresentacao'
+//import BotaoAvancar from './components/BotaoAvancar'
+import TelaRoleta from '../src/pages/TelaRoleta'
 
 function App() {
   return (
     <div className="App">
       <div className="background">
-        <BoxApresentacao />
+        <TelaRoleta/>
+        {/*<BoxApresentacao />
         <Gloria />
-        <BotaoAvancar />
+  <BotaoAvancar />*/}
       </div>
     </div>
   )
--- src/components/BoxApresentacao2.jsx
+++ src/components/BoxApresentacao2.jsx
@@ -14,8 +14,8 @@ function BoxApresentacao2() {
           Então, agora vou te dar uma breve explicação:
           <br></br>
           <br></br>
-          <br></br>
-          Eu tenho a capacidade de interagir com você, fornecendo informações importantes sobre direitos, prevenção e suporte emocional. Estou aqui para ouvir, compartilhar histórias, oferecer orientações e conectar mulheres e meninas a organizações e recursos relevantes. a Orc'estra Gamfiicação me ajudou a tornar esse sonho de trazer essa história de mulheres incríveis para o seu evento maravilhoso do Na Praia, acrescentando a você mais uma experiência extraordinária dentro de tantas que existem nesse evento!
+          
+          Eu tenho a capacidade de interagir com você, fornecendo informações importantes sobre direitos, prevenção e suporte emocional. Estou aqui para ouvir, compartilhar histórias, oferecer orientações e conectar mulheres e meninas a organizações e recursos relevantes. A Orc'estra Gamfiicação me ajudou a tornar esse sonho de trazer essa história de mulheres incríveis para o seu evento maravilhoso do Na Praia, acrescentando a você mais uma experiência extraordinária dentro de tantas que existem nesse evento!
         </h3>
         </div>
     </div>
