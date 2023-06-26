4619f2cb81ce8296274ada151c47c0073c5175c5
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-06-25 22:33:22 -0300

    melhorando texto do convite

--- src/components/BoxInvite.jsx
+++ src/components/BoxInvite.jsx
@@ -7,7 +7,13 @@ function BoxInvite() {
       <div className="box-invite">
         <div className="box-invite-elements">
           <h3 className="box-invite-title">
-          Que tal responder um questionário e testar seu conhecimento quanto a história de mulhres incríveis de Marrocos ? 
+          Você sabia que mais de 40% das mulheres marroquinas entre os 18 e 64 anos já foram vítimas de pelo menos um acto de violência? É por esse e vários outros casos, que estou aqui. <br>
+          </br>
+          <br></br>
+          E para não ficar sozinha nessa missão gostaria de convidar você, que está aproveitando o Na Praia Festival, para me ajudar a conquistar este objetivo.<br>
+          </br><br></br>
+          O que me diz? Vamos juntos contribuir para um mundo melhor para todas elas? 
+
           </h3>
         </div>
       </div>
