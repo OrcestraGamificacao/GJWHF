97151b2ff5797125c64061f2050aa37e723cd12b
MariaAbritta <eduardaabritta@gmail.com>
2023-06-26 01:21:52 -0300

    Finalizando erro de merge

--- src/pages/Invite.js
+++ src/pages/Invite.js
@@ -1,38 +1,16 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 85d3d05 (Merge branch 'Branch-Telas' of https://github.com/OrcestraGamificacao/GJWHF into Branch-Telas)
-import BoxInvite from "../components/BoxInvite";
+import BoxInvite from '../components/BoxInvite'
 import '../styles/BoxInvite.css'
-import BotaoIniciar from "../components/BotaoIniciar";
+import BotaoIniciar from '../components/BotaoIniciar'
 
 const Invite = () => {
-
   return (
-    <div className='invite'>
+    <div className="invite">
       <div className="background-invite">
-          <BoxInvite />
-          <BotaoIniciar />
+        <BoxInvite />
+        <BotaoIniciar />
       </div>
-<<<<<<< HEAD
-=======
-      
->>>>>>> parent of 85d3d05 (Merge branch 'Branch-Telas' of https://github.com/OrcestraGamificacao/GJWHF into Branch-Telas)
     </div>
   )
 }
 
-export default Invite;
-<<<<<<< HEAD
-=======
-const Invite = () => {
-
-  return (
-    <div className='invite'></div>
-  )
-}
-
-export default Invite;
->>>>>>> 45e98f7d4bae060bddbe982b223b5f787022bc5a
-=======
->>>>>>> parent of 85d3d05 (Merge branch 'Branch-Telas' of https://github.com/OrcestraGamificacao/GJWHF into Branch-Telas)
+export default Invite
