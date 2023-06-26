9e67387cc15fa11e3bf0df663c64c285809cb0f0
MariaAbritta <eduardaabritta@gmail.com>
2023-06-26 09:18:41 -0300

    Modificando texto da apresentação2

--- src/components/BoxApresentacao2.jsx
+++ src/components/BoxApresentacao2.jsx
@@ -13,8 +13,7 @@ function BoxApresentacao2() {
           Eu tenho a capacidade de interagir com você, fornecendo informações importantes sobre direitos, prevenção e suporte emocional. Estou aqui para ouvir, compartilhar histórias, oferecer orientações e conectar mulheres e meninas a organizações e recursos relevantes.
           <br></br>
           <br></br>
-          <br></br>
-          Eu tenho a capacidade de interagir com você, fornecendo informações importantes sobre direitos, prevenção e suporte emocional. Estou aqui para ouvir, compartilhar histórias, oferecer orientações e conectar mulheres e meninas a organizações e recursos relevantes. A Orc'estra Gamfiicação me ajudou a tornar esse sonho de trazer essa história de mulheres incríveis para o seu evento maravilhoso do Na Praia, acrescentando a você mais uma experiência extraordinária dentro de tantas que existem nesse evento!
+          Tudo isso através do uso da tecnologia e da gamificação! A gamificação é o uso de elementos e mecânicas de jogos em contextos não relacionados a jogos, com o objetivo de engajar, motivar e envolver as pessoas em determinadas atividades, e assim, espero ajudar a ampliar a voz das mulheres e empoderá-las acima de tudo!  Juntos, podemos criar um impacto e promover mudanças positivas na sociedade, proporcionando segurança, apoio e esperança para todas as mulheres. Junte-se a mim nessa jornada de conhecimento da história de nossas mulheres incríveis!
         </h3>
         </div>
     </div>
