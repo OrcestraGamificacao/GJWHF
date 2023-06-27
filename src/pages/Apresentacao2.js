a71f77979e5f74856996b4be7f307cd4bdd97e74
Luciano Freitas <lucianodefreitasmelo@hotmail.com>
2023-06-26 23:13:40 -0300

    remove gloria apresentacao 2

--- src/pages/Apresentacao2.js
+++ src/pages/Apresentacao2.js
@@ -1,16 +1,12 @@
 import '../styles/BoxApresentacao2.css';
 import BoxApresentacao2 from '../components/BoxApresentacao2';
 import BotaoAvancar2 from '../components/BotaoAvancar2';
-import Gloria from '../components/Gloria';
-import gloriaFalando from '../gloria/gloria_falando.mp4';
 
 const Apresentacao2 = () => {
   return (
     <div className='Apresentacao2'>
       <div className="background-Apresentacao2">
           <BoxApresentacao2 />
-          <Gloria/>
-          <Gloria animacao={gloriaFalando}/>
       </div>
       <div className='botaoAvançar'>
         <BotaoAvancar2 />
