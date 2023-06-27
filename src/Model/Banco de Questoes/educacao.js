7a4b5c0c449014c57738de1cbd95f6845d413e62
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-06-27 19:43:11 -0300

    Adicionando questões de educação.

--- src/Model/Banco de Questoes/educacao.js
+++ src/Model/Banco de Questoes/educacao.js
@@ -1,28 +1,63 @@
 import { Questao } from '../modelQuestao'
 
 // Questoes de educação
-function questoesEducacao(){
-    const eduQ1 = new Questao(1, 'texto introdutorio - educaçao 1', 'Pergunta educaçao 1', '1a - educaçao', '1b - educaçao', '1c - educaçao', '1d - educaçao (certa)', '1d - educaçao (certa)', 'Explicação')
+function questoesEducacao() {
+    const eduQ1 = new Questao(1,
+        'Sana é uma premiada especialista em gênero, defensora das mulheres e palestrante sobre feminismo e questões femininas. Ela é autora de “Mulheres Invisíveis do Oriente Médio”. Ela falou em 50 países em universidades, painéis de discussão e eventos internacionais.  Sana estabeleceu a Iniciativa Womenpreneur que visa promover o lugar das mulheres no cenário empresarial, tecnologia, inovação e sociedade.',
+        'Ela é a co-fundadora da Womenquake. Sobre o que é essa instituição?',
+        'É um movimento global, que revisita e examina criticamente as crenças, mitos e ideias sobre gênero.',
+        'É um movimento que visa quebrar a barreira salarial entre as mulheres e homens.',
+        'É um movimento que visa conscientizar sobre as mudanças climáticas usando palestrantes e cientistas mulheres.',
+        'É um movimento que visa incentivar mulheres a entrarem e estudar cursos de ciência e tecnologia',
+        'É um movimento global, que revisita e examina criticamente as crenças, mitos e ideias sobre gênero.',
+        'Dentro da Womenpreneur foi co-fundadora do Womenquake, um movimento global, que revisita e examina criticamente as crenças, mitos e ideias sobre gênero. Ela usa sua plataforma para promover a agenda de igualdade por meio de histórias, campanhas sociais e ferramentas de compartilhamento para capacitar as mulheres. Essas experiências a levaram a organizar várias campanhas, incluindo a reforma da lei marroquina que permitia que estupradores se casassem com suas vítimas.')
 
-    const eduQ2 = new Questao(2, 'texto introdutorio - educaçao 2', 'Pergunta educaçao 2', '2a - educaçao', '2b - educaçao (certa)', '2c - educaçao', '2d - educaçao', '2b - educaçao (certa)', 'Explicação')
+    const eduQ2 = new Questao(2,
+        'Asmaa Morine Azzouzi depois de se formar no Instituto Superior de Comércio e Administração de Empresas (ISCAE) em Casablanca, após isso Asmaa fez uma pós-graduação em Comércio Internacional e obteve um MBA em Inteligência Econômica e Gestão Estratégica pela Paris School of Economic Warfare (EGE).',
+        'Asmaa criou uma associação, sobre o que é a associação baseado na sua formação?',
+        'Apoiar as mulheres marroquinas na criação e desenvolvimento do seu negócio.',
+        'Instalar incubadoras e fazer advocacia junto ao poder público.',
+        'Ensinar empreendedorismo para crianças pequenas',
+        'Comprar empresas indo à falência e transformar as mesmas em lucro',
+        'Apoiar as mulheres marroquinas na criação e desenvolvimento do seu negócio.',
+        'A Associação de Mulheres Empresárias de Marrocos (AFEM) apoia as mulheres marroquinas na criação e desenvolvimento do seu negócio, apoio que inclui a constituição de incubadoras e sua presidente é Asmaa Morine Azzouzi. Das cerca de 15.000 mulheres líderes empresariais no Reino de Marrocos, 600 aderiram à Associação de Mulheres Empresárias de Marrocos (AFEM).')
 
-    const eduQ3 = new Questao(3, 'texto introdutorio - educaçao 3', 'Pergunta educaçao 3', '3a - educaçao (certa)', '3b - educaçao', '3c - educaçao', '3d - educaçao', '3a - educaçao (certa)', 'Explicação')
+    const eduQ3 = new Questao(3,
+        'Hind Bouqartacha é uma fotógrafa, cinegrafista e proprietária do “GUILE STUDIO”. A fotografia foi para ela uma potente oportunidade criativa de imortalizar momentos, de se expressar livremente e de descobrir o mundo e a si mesma.',
+        'Bouqartacha produziu a série ‘Pedaço de Pão’, que tinha como objetivo ressaltar que tipo de história?',
+        'A história de estudantes marroquinas',
+        'A história do comércio no Marrocos.',
+        'A história de comidas típicas do Marrocos.',
+        'A história de mulheres trabalhadoras.',
+        'A história de mulheres trabalhadoras.',
+        'A série ‘Pedaço de Pão’ de Bouqartacha  mergulha na vida de Milouda, uma padeira que trabalha para sustentar sua família e que construiu sua independência financeira a partir de muita dedicação.')
 
-    const eduQ4 = new Questao(4, 'texto introdutorio - educaçao 4', 'Pergunta educaçao 4', '4a - educaçao', '4b - educaçao', '4c - educaçao (certa)', '4d - educaçao', '4c - educaçao (certa)', 'Explicação')
+    const eduQ4 = new Questao(4,
+        'Com dupla-nacionalidade, francesa e marroquina, Ilham Kadri é graduada em engenharia pela Escola Europeia de Química, Polímeros e Ciência dos Materiais, em Estrasburgo, França, e possui doutorado em físico-química macromolecular pela Universidade Louis Pasteur, em Estrasburgo.',
+        'Como a dupla-nacionalidade de Ilham Kadri pode ter influenciado sua perspectiva científica e suas contribuições para a ciência?',
+        'Facilitando a obtenção de patentes internacionais para suas descobertas.',
+        'Incorporando diferentes abordagens culturais e filosóficas em suas pesquisas.',
+        'Tornando-a mais habilidosa em diferentes técnicas de laboratório.',
+        'Permitindo o acesso a recursos financeiros adicionais para pesquisa.',
+        'Incorporando diferentes abordagens culturais e filosóficas em suas pesquisas.',
+        'Ao ter uma formação e vivência em duas culturas distintas, Ilham Kadri foi exposta a diferentes maneiras de pensar, enxergar o mundo e abordar problemas. Isso pode ter ampliado sua capacidade de pensar de forma mais abrangente e flexível, permitindo que ela aborde questões científicas com uma perspectiva mais diversificada.')
 
-    const eduQ5 = new Questao(5, 'texto introdutorio - educaçao 5', 'Pergunta educaçao 5', '5a - educaçao', '5b - educaçao', '5c - educaçao', '5d - educaçao (certa)', '5d - educaçao (certa)', 'Explicação')
+    // const eduQ5 = new Questao(5, 'texto introdutorio - educaçao 5', 'Pergunta educaçao 5', '5a - educaçao', '5b - educaçao', '5c - educaçao', '5d - educaçao (certa)', '5d - educaçao (certa)', 'Explicação')
 
-    const eduQ6 = new Questao(6, 'texto introdutorio - educaçao 6', 'Pergunta educaçao 6', '6a - educaçao', '6b - educaçao', '6c - educaçao', '6d - educaçao (certa)', '6d - educaçao (certa)', 'Explicação')
+    // const eduQ6 = new Questao(6, 'texto introdutorio - educaçao 6', 'Pergunta educaçao 6', '6a - educaçao', '6b - educaçao', '6c - educaçao', '6d - educaçao (certa)', '6d - educaçao (certa)', 'Explicação')
 
-    const eduQ7 = new Questao(7, 'texto introdutorio - educaçao 7', 'Pergunta educaçao 7', '7a - educaçao', '7b - educaçao (certa)', '7c - educaçao', '7d - educaçao', '7b - educaçao (certa)', 'Explicação')
+    // const eduQ7 = new Questao(7, 'texto introdutorio - educaçao 7', 'Pergunta educaçao 7', '7a - educaçao', '7b - educaçao (certa)', '7c - educaçao', '7d - educaçao', '7b - educaçao (certa)', 'Explicação')
 
-    const eduQ8 = new Questao(8, 'texto introdutorio - educaçao 8', 'Pergunta educaçao 8', '8a - educaçao (certa)', '8b - educaçao', '8c - educaçao', '8d - educaçao', '8a - educaçao (certa)', 'Explicação')
+    // const eduQ8 = new Questao(8, 'texto introdutorio - educaçao 8', 'Pergunta educaçao 8', '8a - educaçao (certa)', '8b - educaçao', '8c - educaçao', '8d - educaçao', '8a - educaçao (certa)', 'Explicação')
 
-    const eduQ9 = new Questao(9, 'texto introdutorio - educaçao 9', 'Pergunta educaçao 9', '9a - educaçao', '9b - educaçao', '9c - educaçao (certa)', '9d - educaçao', '9c - educaçao (certa)', 'Explicação')
+    // const eduQ9 = new Questao(9, 'texto introdutorio - educaçao 9', 'Pergunta educaçao 9', '9a - educaçao', '9b - educaçao', '9c - educaçao (certa)', '9d - educaçao', '9c - educaçao (certa)', 'Explicação')
 
-    const eduQ10 = new Questao(10, 'texto introdutorio - educaçao 10', 'Pergunta educaçao 10', '10a - educaçao', '10b - educaçao', '10c - educaçao', '10d - educaçao (certa)', '10d - educaçao (certa)', 'Você acertou!')
+    // const eduQ10 = new Questao(10, 'texto introdutorio - educaçao 10', 'Pergunta educaçao 10', '10a - educaçao', '10b - educaçao', '10c - educaçao', '10d - educaçao (certa)', '10d - educaçao (certa)', 'Você acertou!')
 
-    const eduQuestoes = [eduQ1, eduQ2, eduQ3, eduQ4, eduQ5, eduQ6, eduQ7, eduQ8, eduQ9, eduQ10]
+    // const eduQuestoes = [eduQ1, eduQ2, eduQ3, eduQ4, eduQ5, eduQ6, eduQ7, eduQ8, eduQ9, eduQ10]
+
+    const eduQuestoes = [eduQ1, eduQ2, eduQ3, eduQ4]
+
 
     return eduQuestoes
 }
