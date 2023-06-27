6c6dbe60c7f4be3cac8b7756ce78d4589ea77303
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-06-27 19:08:01 -0300

    Modificando os textos das telas

--- src/components/BoxApresentacao2.jsx
+++ src/components/BoxApresentacao2.jsx
@@ -8,19 +8,22 @@ function BoxApresentacao2() {
       <div className="box-apresentation2">
         <div className="box-apresentation2-elements">
           <h3 className="box-apresentation2-title">
-            Muito feliz que você aceitou esse desafio! 
+            Muito feliz que você, {sessionStorage.getItem("nome")}, aceitou esse desafio!
             <br></br>
             <br></br>
-            Eu tenho a capacidade de interagir com você, fornecendo informações importantes sobre direitos, prevenção e suporte emocional. Estou aqui para ouvir, compartilhar histórias, oferecer orientações e conectar mulheres e meninas a organizações e recursos relevantes.
+            Eu vou interagir com voce, fornecendo informações importantes sobre <strong style={{color: 'red'}}>(falar sobre os temas)</strong>, te conectando a histórias relevantes!
             <br></br>
             <br></br>
-            Tudo isso através do uso da tecnologia e da gamificação! A gamificação é o uso de elementos e mecânicas de jogos em contextos não relacionados a jogos, com o objetivo de engajar, motivar e envolver as pessoas em determinadas atividades, e assim, espero ajudar a ampliar a voz das mulheres e empoderá-las acima de tudo!  Juntos, podemos criar um impacto e promover mudanças positivas na sociedade, proporcionando segurança, apoio e esperança para todas as mulheres. Junte-se a mim nessa jornada de conhecimento da história de nossas mulheres incríveis!
+            Tudo isso atraves do uso da tecnologia e da gamificação!
+            <br></br>
+            <br></br>
+            A gamificação é o uso de elementos e mecânicas de jogos em contextos não relacionados a jogos, com o objetivo de engajar, motivar e envolver as pessoas em determinadas atividades, e assim, espero ajudar a ampliar a voz das mulheres e empoderá-las acima de tudo! Junte-se a mim nessa jornada de conhecimento da história de nossas mulheres incríveis!
           </h3>
         </div>
         <div className="botaoAvançar-apresentation2">
         <BotaoAvancar2 />
         </div>
-      
+
       </div>
     </div>
 );
--- src/components/BoxInvite.jsx
+++ src/components/BoxInvite.jsx
@@ -36,12 +36,14 @@ function BoxInvite() {
       <div className="box-invite">
         <div className="box-invite-elements">
           <h3 className="box-invite-title">
-          Você sabia que mais de 40% das mulheres marroquinas entre os 18 e 64 anos já foram vítimas de pelo menos um ato de violência? É por esse e vários outros casos, que estou aqui. <br>
+          Você sabia que mais de 27% das mulheres de 15 a 49 anos já foram vítimas de pelo menos um ato de violência? E por esse e vários outros casos que estou aqui. <br>
           </br>
           <br></br>
-          E para não ficar sozinha nessa missão gostaria de convidar você, que está aproveitando o Na Praia Festival, para me ajudar a conquistar este objetivo.<br>
+          E para não ficar sozinha nessa missão gostaria de convidar você para me ajudar a conquistar este objetivo.<br>
           </br><br></br>
           O que me diz? Vamos juntos contribuir para um mundo melhor para todas elas?
+          <br></br>
+          Então começamos com... Qual o seu nome?
           </h3>
           <input value={nome} type="text" className="inputNome" placeholder= "Digite seu nome" id='inputNome' onChange={(e) => handleInputChange(e)}/>
           <button onClick={changeUrl} className="grow_ellipse">SÓ SE FOR AGORA</button>
