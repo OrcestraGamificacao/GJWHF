c9e29a681ba2117fb61e51b1e3197cd855a047a5
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-06-27 19:17:49 -0300

    Adicionando perguntas de inovação.

--- src/Model/Banco de Questoes/inovacao.js
+++ src/Model/Banco de Questoes/inovacao.js
@@ -1,28 +1,93 @@
 import { Questao } from "../modelQuestao"
 
 // Questoes de inovação
-function questoesInovacao(){
-    const inovQ1 = new Questao(1, 'texto introdutorio - inovação 1', 'Pergunta inovação 1', '1a - inovação', '1b - inovação', '1c - inovação', '1d - inovação (certa)', '1d - inovação (certa)', 'Explicação')
+function questoesInovacao() {
+    const inovQ1 = new Questao(1,
+        'A marroquina Meriem Belkhayat é uma designer renomada que possui criações únicas e típicas do Oriente, que são produzidas com os melhores tecidos e cristais existentes, de maneira 100% artesanal.',
+        'Que tipo de produto Meriem confecciona para mulheres?',
+        'Confecciona roupas íntimas.',
+        'Confecciona kaftans (túnicas longas).',
+        'Confecciona cintas modeladoras.',
+        'Confecciona kimonos.',
+        '1d - inovação (certa)',
+        'Considerada uma das mais célebres designers de kaftan em Marrocos e no mundo árabe, os designs de Meriem Belkhayat são obras-primas que prezam pela autenticidade e elegância marroquina com um toque moderno. Seu trabalho árduo, sua criatividade e sua dedicação com seu trabalho são os segredos para o seu crescente sucesso em uma sociedade nada propícia.')
 
-    const inovQ2 = new Questao(2, 'texto introdutorio - inovação 2', 'Pergunta inovação 2', '2a - inovação', '2b - inovação (certa)', '2c - inovação', '2d - inovação', '2b - inovação (certa)', 'Explicação')
+    const inovQ2 = new Questao(2,
+        'Fundadora do centro de tecnologia Douar Tech, Hanae Bezad tem como foco a inclusão de jovens, principalmente mulheres, de origens precárias em áreas rurais e semi-urbanas, no empreendedorismo de startups.',
+        'Qual é o objetivo principal da organização de Hanae?',
+        'Criar oportunidades de liderança feminina em organizações comunitárias.',
+        'Oferecer oportunidades de emprego para mulheres em áreas rurais e semi-urbanas',
+        'Colaborar para uma educação maior de mulheres de origens precárias.',
+        'Contribuir para a inclusão de mulheres no desenvolvimento da economia e tecnologia no país.',
+        '2b - inovação (certa)',
+        'Sua organização sem fins lucrativos promove a independência econômica de jovens, especialmente das mulheres, provenientes de meios precários de zonas rurais, capacitando-os com formação em empreendedorismo inovador e tecnologia.')
 
-    const inovQ3 = new Questao(3, 'texto introdutorio - inovação 3', 'Pergunta inovação 3', '3a - inovação (certa)', '3b - inovação', '3c - inovação', '3d - inovação', '3a - inovação (certa)', 'Explicação')
+    const inovQ3 = new Questao(3,
+        'Fadwa Moussaif é a fundadora da Boucharouette Eco-Création (B.E.C), uma empresa que começou como um projeto escolar onde os estudantes receberam a atividade de criar uma empresa com soluções de cunho social.',
+        'Em que ramo a empresa de Fadwa Moussaif atua?',
+        'Têxtil, na fabricação de túnicas.',
+        'Têxtil, na fabricação de tapetes.',
+        'Têxtil, na fabricação de acessórios de moda e artesanato.',
+        'Têxtil, na fabricação de Hijab.',
+        'Têxtil, na fabricação de acessórios de moda e artesanato.',
+        'Boucharouette Eco-Création é uma empresa especializada na reciclagem de tecidos tecidos e feitos à mão para transformá-los em acessórios de moda, como bolsas, e artesanato, como capas de almofadas.')
 
-    const inovQ4 = new Questao(4, 'texto introdutorio - inovação 4', 'Pergunta inovação 4', '4a - inovação', '4b - inovação', '4c - inovação (certa)', '4d - inovação', '4c - inovação (certa)', 'Explicação')
+    const inovQ4 = new Questao(4,
+        'Fatim-Zahra trabalhou vários anos como consultor de negócios em Paris para multinacionais líderes e especializou-se em gerenciamento de projetos, estratégia de gerenciamento de mudanças. Ela também realizou estudos sobre a experiência do cliente.',
+        'Com base nisso, qual iniciativa Fatim-Zahra construiu no Marrocos que aproveita toda sua experiência no meio empresarial?',
+        'Aceleradora de Startups.',
+        'Agência de crédito para empresas.',
+        'Consultoria de direito empresarial.',
+        'Escola de empreendedorismo.',
+        'Aceleradora de Startups.',
+        'Fatim-Zahra lançou o New Work Lab que é um ambiente positivo e criativo construído para despertar o espírito empreendedor e disseminar a cultura startup. Dezenas de programas de apoio desenhados e lançados, 15.000 participantes hospedados, 300 empreendedores acompanhados, 60 startups aceleradas. O New Work Lab se tornou o local da comunidade de startups em Casablanca e está desempenhando um papel importante no cenário de startups.')
 
-    const inovQ5 = new Questao(5, 'texto introdutorio - inovação 5', 'Pergunta inovação 5', '5a - inovação', '5b - inovação', '5c - inovação', '5d - inovação (certa)', '5d - inovação (certa)', 'Explicação')
+    const inovQ5 = new Questao(5,
+        'Fundada por Rania Belkahia, a Afrimarket tem como objetivo principal proporcionar acesso a produtos básicos e essenciais para os consumidores que enfrentam desafios logísticos e de acesso em muitas partes da África.',
+        'De que forma o e-commerce de Rania pode melhorar a vida de milhões de africanos?',
+        'Oferecendo promoções de produtos de luxo.',
+        'Promovendo a exportação de produtos africanos para outros continentes.',
+        'Estabelecendo pontos de coleta para retirada pessoal dos produtos.',
+        'Minimizando os custos de frete para os clientes.',
+        'Estabelecendo pontos de coleta para retirada pessoal dos produtos.',
+        'Ao estabelecer pontos de coleta estrategicamente localizados, o Afrimarket proporciona aos clientes a oportunidade de retirar seus produtos pessoalmente em locais convenientes, mesmo que a entrega em domicílio seja difícil ou demorada.')
 
-    const inovQ6 = new Questao(6, 'texto introdutorio - inovação 6', 'Pergunta inovação 6', '6a - inovação', '6b - inovação', '6c - inovação', '6d - inovação (certa)', '6d - inovação (certa)', 'Explicação')
+    const inovQ6 = new Questao(6,
+        'A empresária marroquina Bouchra Ezzahraoui inovou ao fundar a empresa de joias AUrate, em 2017, que possui criações únicas. A empresa contou com investidores que a ajudaram na construção e na elevação da marca à fama internacional e chegou a ser usada por nomes como Julia Roberts, Chrissy Teigen e Priyanka Chopra.',
+        'Qual o impacto da associação da marca com personalidades femininas de renome?',
+        'Reforça estereótipos de gênero e limita o papel das mulheres na sociedade.',
+        'Impacta na reputação da empresa, pois pode ser interpretado como marketing de celebridades superficial.',
+        'Fortalece a imagem e o reconhecimento da marca como um símbolo de empoderamento feminino.',
+        'Pode provocar a diminuição da demanda pelos produtos da empresa.',
+        'Fortalece a imagem e o reconhecimento da marca como um símbolo de empoderamento feminino.',
+        'O uso dessas personalidades femininas como embaixadoras da marca amplia a visibilidade e a influência da representatividade feminina em diversos setores. Ao estarem associadas à marca de Bouchra, essas celebridades inspiram outras mulheres a seguirem carreiras empreendedoras e a lutarem pela igualdade de gênero.')
 
-    const inovQ7 = new Questao(7, 'texto introdutorio - inovação 7', 'Pergunta inovação 7', '7a - inovação', '7b - inovação (certa)', '7c - inovação', '7d - inovação', '7b - inovação (certa)', 'Explicação')
+    const inovQ7 = new Questao(7,
+        'Sophia Alj fundou, junto de seu marido, a empresa Chari.co, um aplicativo que foi avaliado em 100 bilhões de dólares após ser lançado.',
+        'Sobre o que é a plataforma de Sophia?',
+        'Aluguéis.',
+        'Revendas.',
+        'Vendas.',
+        'Reservas.',
+        'Revendas.',
+        'O aplicativo de Sophia conecta revendedores em países que possuem como idioma oficial o francês na África a companhias em outras regiões do planeta, buscando financiar mais de 200 mil donos de lojas em Marrocos, para que possam expandir seus negócios.')
 
-    const inovQ8 = new Questao(8, 'texto introdutorio - inovação 8', 'Pergunta inovação 8', '8a - inovação (certa)', '8b - inovação', '8c - inovação', '8d - inovação', '8a - inovação (certa)', 'Explicação')
+    const inovQ8 = new Questao(8,
+        'Hanae Bezad é a fundadora da Douar Tech, um centro de tecnologia inclusiva que ajuda a capacitar jovens e mulheres com habilidades digitais em áreas rurais e semi-urbanas em Marrocos, também já trabalhou como  gerente de projetos em startups e ecossistemas TIC para  a Smart Africa.',
+        'De que forma mulheres como Hanae Bezad contribuem para a revolução digital na África?',
+        'Contribui para a promoção da alfabetização digital entre as comunidades urbanas.',
+        'As mulheres têm habilidades técnicas superiores em comparação com os homens.',
+        'Criando plataformas online para promover negócios de mulheres.',
+        'A participação das mulheres ajuda a acelerar o desenvolvimento e reforça a narrativa tecnológica na África.',
+        'A participação das mulheres ajuda a acelerar o desenvolvimento e reforça a narrativa tecnológica na África.',
+        'A participação das mulheres desempenha um papel crucial na aceleração do desenvolvimento e no fortalecimento da tecnologia na África. Ao serem capacitadas e terem acesso igualitário às oportunidades digitais, elas contribuem de maneira significativa para o crescimento econômico e social do continente.')
 
-    const inovQ9 = new Questao(9, 'texto introdutorio - inovação 9', 'Pergunta inovação 9', '9a - inovação', '9b - inovação', '9c - inovação (certa)', '9d - inovação', '9c - inovação (certa)', 'Explicação')
+    // const inovQ9 = new Questao(9, 'texto introdutorio - inovação 9', 'Pergunta inovação 9', '9a - inovação', '9b - inovação', '9c - inovação (certa)', '9d - inovação', '9c - inovação (certa)', 'Explicação')
 
-    const inovQ10 = new Questao(10, 'texto introdutorio - inovação 10', 'Pergunta inovação 10', '10a - inovação', '10b - inovação', '10c - inovação', '10d - inovação (certa)', '10d - inovação (certa)', 'Explicação')
+    // const inovQ10 = new Questao(10, 'texto introdutorio - inovação 10', 'Pergunta inovação 10', '10a - inovação', '10b - inovação', '10c - inovação', '10d - inovação (certa)', '10d - inovação (certa)', 'Explicação')
 
-    const inovQuestoes = [inovQ1, inovQ2, inovQ3, inovQ4, inovQ5, inovQ6, inovQ7, inovQ8, inovQ9, inovQ10]
+    // const inovQuestoes = [inovQ1, inovQ2, inovQ3, inovQ4, inovQ5, inovQ6, inovQ7, inovQ8, inovQ9, inovQ10]
+    const inovQuestoes = [inovQ1, inovQ2, inovQ3, inovQ4, inovQ5, inovQ6, inovQ7, inovQ8]
 
     return inovQuestoes
 
