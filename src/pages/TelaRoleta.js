4941d801363483490b722a5b3935015bcfcf70d1
Maria Alice <marialice3003@gmail.com>
2023-06-27 11:45:01 -0300

    responsividade carrossel

--- src/pages/TelaRoleta.js
+++ src/pages/TelaRoleta.js
@@ -7,7 +7,7 @@ const TelaRoleta = () => {
   return (
     <div className='TelaRoleta'>
       <div className="background-roleta">
-        <h1 style={{marginLeft: "30%", marginRight: "30%"}} className="titulo">Gire a roleta e encontre qual será o tema em que você irá descobrir mais sobre o mundo do protagonismo feminino no Marrocos</h1>
+        <h1 style={{padding: "5%"}} className="titulo">Gire a roleta e encontre qual será o tema em que você irá descobrir mais sobre o mundo do protagonismo feminino no Marrocos</h1>
         <div className="Roleta">
           <Roleta/>
         </div>
