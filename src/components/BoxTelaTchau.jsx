28d72f4f6263b0a129999778c7c40e0824faf515
Amanda De Moura Da Silva <110147898+AmandaMourx@users.noreply.github.com>
2023-06-28 03:26:14 -0300

    Box da tela tchau

---
+++ src/components/BoxTelaTchau.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles/BoxTelaTchau.css";
+
+function BoxTelaTchau() {
+  return (
+    <div>
+      <div className="box-TelaTchau">
+        <div className="box-TelaTchau-elements">
+        
+        </div>
+      </div>
+      </div>
+);
+}
+
+export default BoxTelaTchau;
