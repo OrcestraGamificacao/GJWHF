671761872add0cb65ddccd1b4ec83965d4ac957f
Camila Careli <camilacsouzza@gmail.com>
2023-06-29 16:40:12 -0300

    Perguntas inseridas e resisadas dos temas Saúde e Inovacao

--- src/Model/Banco de Questoes/inovacao.js
+++ src/Model/Banco de Questoes/inovacao.js
@@ -14,7 +14,7 @@ function questoesInovacao() {
 
     const inovQ2 = new Questao(2,
         'Fundadora do centro de tecnologia Douar Tech, Hanae Bezad tem como foco a inclusão de jovens, principalmente mulheres, de origens precárias em áreas rurais e semi-urbanas, no empreendedorismo de startups.',
-        'Qual é o objetivo principal da organização de Hanae?',
+        'Qual é o objetivo principal da organização de Hanae Bezad?',
         'Criar oportunidades de liderança feminina em organizações comunitárias',
         'Oferecer oportunidades de emprego para mulheres em áreas rurais e semi-urbanas',
         'Colaborar para uma educação maior de mulheres de origens precárias',
@@ -23,6 +23,16 @@ function questoesInovacao() {
         'Sua organização sem fins lucrativos promove a independência econômica de jovens, especialmente das mulheres, provenientes de meios precários de zonas rurais, capacitando-os com formação em empreendedorismo inovador e tecnologia.')
 
     const inovQ3 = new Questao(3,
+        'Saloua Karkri Belkeziz  é uma empresária na área de Tecnologias da Informação e Comunicação (TIC). Atualmente, é Vice-presidente da Inetum Africa, ex-GFI, e fundadora da Associação de Mulheres Empresárias em Marrocos (AFEM).',
+        'Em 2000, Saloua Karkri Belkeziz fundou a Associação de Mulheres Empresárias em Marrocos (AFEM), a primeira no País. Qual a principal missão desta associação?',
+        'Facilitar o acesso a tecnologias para empreendedoras',
+        'Aumentar o número de empresas no Marrocos',
+        'Promover o aumento econômico do país a partir da criação de empresas  com tecnologias inovadoras criadas por mulheres',
+        'Promover o empreendedorismo feminino',
+        'Promover o empreendedorismo feminino',
+        'A Associação de Mulheres Empresárias em Marrocos (AFEM), foi fundada por Belkeziz, depois da observação da baixa presença de mulheres empresárias em organizações profissionais, de forma a ser seu principal objetivo promover o empreendedorismo feminino e incentivar a criação de empresas por mulheres')
+
+    const inovQ4 = new Questao(4,
         'Fadwa Moussaif é a fundadora da Boucharouette Eco-Création (B.E.C), uma empresa que começou como um projeto escolar onde os estudantes receberam a atividade de criar uma empresa com soluções de cunho social.',
         'Em que ramo a empresa de Fadwa Moussaif atua?',
         'Têxtil, na fabricação de túnicas',
@@ -32,29 +42,39 @@ function questoesInovacao() {
         'Têxtil, na fabricação de acessórios de moda e artesanato',
         'Boucharouette Eco-Création é uma empresa especializada na reciclagem de tecidos tecidos e feitos à mão para transformá-los em acessórios de moda, como bolsas, e artesanato, como capas de almofadas.')
 
-    const inovQ4 = new Questao(4,
+    const inovQ5 = new Questao(5,
         'Fatim-Zahra trabalhou vários anos como consultor de negócios em Paris para multinacionais líderes e especializou-se em gerenciamento de projetos, estratégia de gerenciamento de mudanças. Ela também realizou estudos sobre a experiência do cliente.',
-        'Com base nisso, qual iniciativa Fatim-Zahra construiu no Marrocos que aproveita toda sua experiência no meio empresarial?',
-        'Aceleradora de Startups',
+        'Qual iniciativa Fatim-Zahra construiu no Marrocos que aproveita toda sua experiência no meio empresarial?',
+        'Aceleradora de Startup',
         'Agência de crédito para empresas',
         'Consultoria de direito empresarial',
         'Escola de empreendedorismo',
-        'Aceleradora de Startups',
+        'Aceleradora de Startup',
         'Fatim-Zahra lançou o New Work Lab que é um ambiente positivo e criativo construído para despertar o espírito empreendedor e disseminar a cultura startup. Dezenas de programas de apoio desenhados e lançados, 15.000 participantes hospedados, 300 empreendedores acompanhados, 60 startups aceleradas. O New Work Lab se tornou o local da comunidade de startups em Casablanca e está desempenhando um papel importante no cenário de startups.')
 
-    const inovQ5 = new Questao(5,
+    const inovQ6 = new Questao(6,
+        'Ilham Kadri é a atual CEO da Solvay, uma empresa de materiais avançados e de especialidades químicas, a organização é comprometida com o desenvolvimento da Química e aborda os principais desafios da sociedade.',
+        ' Qual a importância da participação de mulheres como Ilham Kadri, na busca por soluções para desafios globais?',
+        'Elas podem trazer abordagens mais holísticas e abrangentes',
+        'Aumentando o viés de gênero no campo',
+        'Reforço das normas tradicionais de gênero',
+        'Aumento da homogeneidade nas decisões',
+        'Elas podem trazer abordagens mais holísticas e abrangentes',
+        'Com a participação de mulheres, suas perspectivas únicas e experiências pessoais podem enriquecer a tomada de decisões e promover soluções mais abrangentes e eficazes para desafios globais.')
+
+    const inovQ7 = new Questao(7,
         'Fundada por Rania Belkahia, a Afrimarket tem como objetivo principal proporcionar acesso a produtos básicos e essenciais para os consumidores que enfrentam desafios logísticos e de acesso em muitas partes da África.',
         'De que forma o e-commerce de Rania pode melhorar a vida de milhões de africanos?',
         'Oferecendo promoções de produtos de luxo',
         'Promovendo a exportação de produtos africanos para outros continentes',
         'Estabelecendo pontos de coleta para retirada pessoal dos produtos',
-        'Minimizando os custos de frete para os clientes',
+        'Minimizando os custos de frete para os clientes.',
         'Estabelecendo pontos de coleta para retirada pessoal dos produtos',
         'Ao estabelecer pontos de coleta estrategicamente localizados, o Afrimarket proporciona aos clientes a oportunidade de retirar seus produtos pessoalmente em locais convenientes, mesmo que a entrega em domicílio seja difícil ou demorada.')
 
-    const inovQ6 = new Questao(6,
+    const inovQ8 = new Questao(8,
         'A empresária marroquina Bouchra Ezzahraoui inovou ao fundar a empresa de joias AUrate, em 2017, que possui criações únicas. A empresa contou com investidores que a ajudaram na construção e na elevação da marca à fama internacional e chegou a ser usada por nomes como Julia Roberts, Chrissy Teigen e Priyanka Chopra.',
-        'Qual o impacto da associação da marca com personalidades femininas de renome?',
+        'Qual o impacto da associação da marca de Bouchra Ezzahraoui com personalidades femininas de renome?',
         'Reforça estereótipos de gênero e limita o papel das mulheres na sociedade',
         'Impacta na reputação da empresa, pois pode ser interpretado como marketing de celebridades superficial',
         'Fortalece a imagem e o reconhecimento da marca como um símbolo de empoderamento feminino',
@@ -62,31 +82,34 @@ function questoesInovacao() {
         'Fortalece a imagem e o reconhecimento da marca como um símbolo de empoderamento feminino',
         'O uso dessas personalidades femininas como embaixadoras da marca amplia a visibilidade e a influência da representatividade feminina em diversos setores. Ao estarem associadas à marca de Bouchra, essas celebridades inspiram outras mulheres a seguirem carreiras empreendedoras e a lutarem pela igualdade de gênero.')
 
-    const inovQ7 = new Questao(7,
-        'Sophia Alj fundou, junto de seu marido, a empresa Chari.co, um aplicativo que foi avaliado em 100 bilhões de dólares após ser lançado.',
-        'Sobre o que é a plataforma de Sophia?',
-        'Aluguéis',
-        'Revendas',
-        'Vendas',
-        'Reservas',
-        'Revendas',
-        'O aplicativo de Sophia conecta revendedores em países que possuem como idioma oficial o francês na África a companhias em outras regiões do planeta, buscando financiar mais de 200 mil donos de lojas em Marrocos, para que possam expandir seus negócios.')
+    const inovQ9 = new Questao(9, 'Sophia Alj fundou, junto de seu marido, a empresa Chari.co, um aplicativo que foi avaliado em 100 bilhões de dólares após ser lançado.',
+    'Sobre o que é a plataforma de Sophia Alj?',
+    'Aluguéis',
+    'Revendas',
+    'Vendas',
+    'Reservas',
+    'Revendas',
+    'O aplicativo de Sophia conecta revendedores em países que possuem como idioma oficial o francês na África a companhias em outras regiões do planeta, buscando financiar mais de 200 mil donos de lojas em Marrocos, para que possam expandir seus negócios.')
 
-    const inovQ8 = new Questao(8,
-        'Hanae Bezad é a fundadora da Douar Tech, um centro de tecnologia inclusiva que ajuda a capacitar jovens e mulheres com habilidades digitais em áreas rurais e semi-urbanas em Marrocos, também já trabalhou como  gerente de projetos em startups e ecossistemas TIC para  a Smart Africa.',
-        'De que forma mulheres como Hanae Bezad contribuem para a revolução digital na África?',
-        'Contribui para a promoção da alfabetização digital entre as comunidades urbanas',
-        'As mulheres têm habilidades técnicas superiores em comparação com os homens',
-        'Criando plataformas online para promover negócios de mulheres',
-        'A participação das mulheres ajuda a acelerar o desenvolvimento e reforça a narrativa tecnológica na África',
-        'A participação das mulheres ajuda a acelerar o desenvolvimento e reforça a narrativa tecnológica na África',
-        'A participação das mulheres desempenha um papel crucial na aceleração do desenvolvimento e no fortalecimento da tecnologia na África. Ao serem capacitadas e terem acesso igualitário às oportunidades digitais, elas contribuem de maneira significativa para o crescimento econômico e social do continente.')
+    const inovQ10 = new Questao(10, 'Hanae Bezad é a fundadora da Douar Tech, um centro de tecnologia inclusiva que ajuda a capacitar jovens e mulheres com habilidades digitais em áreas rurais e semi-urbanas em Marrocos, também já trabalhou como  gerente de projetos em startups e ecossistemas TIC para  a Smart Africa.',
+    'De que forma mulheres como Hanae Bezad contribuem para a revolução digital na África?',
+    'Contribui para a promoção da alfabetização digital entre as comunidades urbanas',
+    'As mulheres têm habilidades técnicas superiores em comparação com os homens',
+    'Criando plataformas online para promover negócios de mulheres',
+    'A participação das mulheres ajuda a acelerar o desenvolvimento e reforça a narrativa tecnológica na África',
+    'A participação das mulheres ajuda a acelerar o desenvolvimento e reforça a narrativa tecnológica na África',
+    'A participação das mulheres desempenha um papel crucial na aceleração do desenvolvimento e no fortalecimento da tecnologia na África. Ao serem capacitadas e terem acesso igualitário às oportunidades digitais, elas contribuem de maneira significativa para o crescimento econômico e social do continente.')
 
-    const inovQ9 = new Questao(9, 'texto introdutorio - inovação 9', 'Pergunta inovação 9', '9a - inovação', '9b - inovação', '9c - inovação (certa)', '9d - inovação', '9c - inovação (certa)', 'Explicação')
+    const inovQ11 = new Questao(10, 'Rita possui uma fundação, em atividade desde maio de 1982, dedicada a ajudar no processo de adoção. Hoje, a Associação apoia integralmente cerca de 350 crianças abandonadas à nascença.',
+    'Rita Zniber descobriu uma falha de cuidado que levava à morte dos bebês órfãos recém nascidos em um hospital marroquino. Escolha a alternativa que você acha que descreve como Rita agiu diante da situação com a sua fundação:',
+    'Rita processou o hospital, com uma alegação de infração aos direitos humanos e das crianças',
+    'Rita protestou em frente ao hospital por dias com apoiadores para o hospital pagar uma multa',
+    'Rita criou um espaço no hospital para cuidar dessas crianças que tinham sido abandonadas',
+    'Rita comprou o hospital e treinou todos os funcionários para cuidar melhor dessas crianças em situação de risco',
+    'Rita criou um espaço no hospital para cuidar dessas crianças que tinham sido abandonadas',
+    'O NID, é o nome do Centro que Rita Zniber criou. Teve uma colaboração com o hospital muito difícil, por múltiplas razões, mas mesmo assim conseguiu salvar da morte 23 crianças que não tinham sido adotadas! O mais velho dos quais tinha 19 meses. E depois o NID foi expandido para ajudar em várias áreas pediátricas de crianças de várias idades.')
 
-    const inovQ10 = new Questao(10, 'texto introdutorio - inovação 10', 'Pergunta inovação 10', '10a - inovação', '10b - inovação', '10c - inovação', '10d - inovação (certa)', '10d - inovação (certa)', 'Explicação')
-
-    const inovQuestoes = [inovQ1, inovQ2, inovQ3, inovQ4, inovQ5, inovQ6, inovQ7, inovQ8, inovQ9, inovQ10]
+    const inovQuestoes = [inovQ1, inovQ2, inovQ3, inovQ4, inovQ5, inovQ6, inovQ7, inovQ8, inovQ9, inovQ10, inovQ11]
 
     return inovQuestoes
 
--- src/Model/Banco de Questoes/saude.js
+++ src/Model/Banco de Questoes/saude.js
@@ -4,17 +4,17 @@ import { Questao } from "../modelQuestao"
 function questoesSaude() {
     const saudeQ1 = new Questao(1,
         'Lamia Tazi, é presidente e diretora-geral da Sothema, uma das principais empresas farmacêuticas do Oriente Médio e da África. Também é diretora da Fundação Omar, diretora-geral da West Afric Pharma no Senegal e vice-presidente da Federação Marroquina da Indústria Farmacêutica.',
-        'Que impacto causa uma mulher em cargos de liderança tão altos como os citados?',
+        'Que impacto causa uma mulher em cargos de liderança tão altos como os ocupados por Lamia Tazi?',
         'Inspira e encoraja outras mulheres a buscar posições de liderança',
         'Cria um ambiente de competição desleal entre homens e mulheres',
         'Impulsiona o progresso mas inibe a inovação na área',
         'Estimula a igualdade salarial, mas cria uma atmosfera de competitividade prejudicial entre as mulheres',
         'Inspira e encoraja outras mulheres a buscar posições de liderança',
-        'A frase da juíza Edssandra Barbosa Lourenço exemplifica muito bem a importância da ocupação destes cargos: “Não é uma questão de status, poder, ou competição entre gêneros, mas da necessária ocupação de espaços que também pertencem a nós".')
+        'A frase da juíza Edssandra Barbosa Lourenço exemplifica muito bem a importância da ocupação destes cargos: “Não é uma questão de status, poder, ou competição entre gêneros, mas da necessária ocupação de espaços que também pertencem a nós.”')
 
     const saudeQ2 = new Questao(2,
         'Doutora em Farmácia, Lamia Tazi contribuiu significativamente para o desenvolvimento e modernização de uma das principais empresas farmacêuticas do Oriente Médio e da África. Graças ao seu compromisso com o desenvolvimento do ensino e da investigação científica, foi eleita em 2019 para integrar o Conselho de Administração da Agência Nacional de Avaliação e Garantia da Qualidade do Ensino Superior e da Investigação Científica.',
-        'Qual é a importância das nomeações de Lamia para a representatividade feminina na ciência?',
+        'Qual é a importância das nomeações de Lamia Tazi para a representatividade feminina na ciência?',
         'Estimula a criação de políticas regulatórias para a indústria farmacêutica no Marrocos',
         'Demonstra a igualdade de oportunidades de liderança para quaisquer gêneros na indústria farmacêutica',
         'Reforça a falta de necessidade da diversidade de gênero no setor farmacêutico',
@@ -23,6 +23,16 @@ function questoesSaude() {
         'Ao envolver mulheres na definição de políticas educacionais e científicas, é possível criar ambientes e programas que atendam às suas necessidades específicas, promovendo igualdade de oportunidades e removendo barreiras que possam impedi-las de acessar a educação e a ciência em pé de igualdade com os homens.')
 
     const saudeQ3 = new Questao(3,
+        ' Ilham Kadri foi a fundadora da rede ISSA Hygieia, uma comunidade dedicada ao avanço e retenção de mulheres em todas as filiais e em todos os níveis da indústria global de limpeza, com foco especial no avanço dos produtos de limpeza.',
+        'De que maneira a organização de Ilham Kadri pode impactar e auxiliar na carreira de mulheres?',
+        'Exigindo que as mulheres desempenhem papéis tradicionais de gênero',
+        ' Limitando as oportunidades de liderança apenas para mulheres',
+        'Fornecendo mentoria e oportunidades de desenvolvimento profissional',
+        'Proporcionar oportunidades de treinamento exclusivas para as mulheres',
+        'Fornecendo mentoria e oportunidades de desenvolvimento profissional',
+        'Fornecer mentoria e oportunidades de desenvolvimento profissional é importante para o crescimento e sucesso das mulheres no ambiente de trabalho. Pois oferece orientação, suporte e aconselhamento de profissionais experientes, permitindo que mulheres aprendam com suas experiências e se desenvolvam em suas carreiras.')
+
+    const saudeQ4 = new Questao(4,
         'Nascida no Marrocos, Mouna Abbassy cresceu em um país onde os rituais de beleza faziam parte dos assuntos sociais essenciais para as mulheres. Ela testemunhou sua mãe e outras mulheres da comunidade prepararem suas próprias receitas de beleza com ingredientes crus e naturais.',
         'Mouna Abbassy é:',
         'Uma influenciadora de beleza de Dubai que promove produtos de marcas marroquinas',
@@ -30,31 +40,28 @@ function questoesSaude() {
         'Uma empresária marroquina que lançou uma marca de beleza natural',
         'Uma ativista que criou uma fundação que mostra a importância da estética para as mulheres',
         'Uma empresária marroquina que lançou uma marca de beleza natural',
-        'Mouna Abbassy fundou a Izil em 2013 após uma carreira de marketing de sucesso em empresas multinacionais de prestígio em Dubai. Sua paixão e seu compromisso com a excelência lhe renderam muitos prêmios de prestígio, como o Prêmio Hadafi e o prêmio Cartier Women`s Initiative, e um lugar entre as 5 melhores mulheres nos negócios pela Arabian Business Startup e entre as 5 mulheres árabes que estão mudando o jogo da beleza pela Vogue Oriente Médio.')
+        "Mouna Abbassy fundou a Izil em 2013 após uma carreira de marketing de sucesso em empresas multinacionais de prestígio em Dubai. Sua paixão e seu compromisso com a excelência lhe renderam muitos prêmios de prestígio, como o Prêmio Hadafi e o prêmio Cartier Women's Initiative, e um lugar entre as 5 melhores mulheres nos negócios pela Arabian Business Startup e entre as 5 mulheres árabes que estão mudando o jogo da beleza pela Vogue Oriente Médio.")
 
-    const saudeQ4 = new Questao(4,
-        'Rita Zniber é presidente e CEO da Diana Holding, é reconhecida por suas diversas parcerias estratégicas e por seus acordos de investimento, como a aquisição da Atlas Bottling Company em parceria com a Equatorial Coca-Cola Bottling Company. Suas aquisições a fizeram aparecer na lista da Forbes das 100 mulheres mais poderosas do Oriente Médio.',
-        'Qual critério pode ter sido o responsável pela aparição de Rita Zniber na lista da Forbes?',
+    const saudeQ5 = new Questao(5, 
+        'Rita Zniber é presidente e CEO da Diana Holding, é reconhecida por suas diversas parcerias estratégicas e por seus acordos de investimento, como a aquisição da Atlas Bottling Company em parceria com a Equatorial Coca-Cola Bottling Company. Suas aquisições a fizeram aparecer na lista da Forbes das 100 mulheres mais poderosas do Oriente Médio.', 
+        'Qual critério pode ter sido o responsável pela aparição de Rita Zniber na lista da Forbes?', 
         'Filantropia e trabalho social',
-        'Inovação e empreendedorismo',
+        'Inovação e empreendedorismo', 
         'Sustentabilidade empresarial',
         'Contribuição para a área da saúde',
         'Inovação e empreendedorismo',
         'Zniber demonstrou habilidades empreendedoras ao liderar a empresa em diferentes setores, desde alimentos e bebidas até plásticos e distribuição. Sua capacidade de identificar oportunidades de negócios e implementar estratégias inovadoras contribuiu para o crescimento e sucesso da empresa.')
 
-    const saudeQ5 = new Questao(5, 'texto introdutorio - saude 5', 'Pergunta saude 5', '5a - saude', '5b - saude', '5c - saude', '5d - saude (certa)', '5d - saude (certa)', 'Você acertou!', 'Explicação')
+    const saudeQ6 = new Questao(6, 
+        'A marroquina Mouna Abbassy fundou sua marca de produtos para cuidados, totalmente naturais, há 10 anos, inspirada em receitas antigas marroquinas. Em 2021, Abassy abriu novos escritórios em Dubai e lançou um Hammam e Spa marroquino no Dubai Mall.', 'Quais tipos de produtos Mouna Abbassy comercializa?', 
+        'Produtos apenas para cabelo', 
+        'Produtos para maquiagem',
+        'Produtos para unhas',
+        'Produtos para pele, cabelo e corpo.',
+        'Produtos para pele, cabelo e corpo', 
+        'Abbassy criou sua linha de produtos para cuidados com a pele, totalmente naturais, há 10 anos, inspirada em receitas antigas marroquinas. Hoje, a marca oferece produtos para a pele, cabelo e corpo. Os Estados Unidos e o Reino Unido são os maiores mercados da marca fora do GCC, registrando um crescimento de 40% e 50% nas vendas online em 2021, respectivamente.')
 
-    const saudeQ6 = new Questao(6, 'texto introdutorio - saude 6', 'Pergunta saude 6', '6a - saude', '6b - saude', '6c - saude', '6d - saude (certa)', '6d - saude (certa)', 'Você acertou!', 'Explicação')
-
-    const saudeQ7 = new Questao(7, 'texto introdutorio - saude 7', 'Pergunta saude 7', '7a - saude', '7b - saude (certa)', '7c - saude', '7d - saude', '7b - saude (certa)', 'Você acertou!', 'Explicação')
-
-    const saudeQ8 = new Questao(8, 'texto introdutorio - saude 8', 'Pergunta saude 8', '8a - saude (certa)', '8b - saude', '8c - saude', '8d - saude', '8a - saude (certa)', 'Você acertou!', 'Explicação')
-
-    const saudeQ9 = new Questao(9, 'texto introdutorio - saude 9', 'Pergunta saude 9', '9a - saude', '9b - saude', '9c - saude (certa)', '9d - saude', '9c - saude (certa)', 'Você acertou!', 'Explicação')
-
-    const saudeQ10 = new Questao(10, 'texto introdutorio - saude 10', 'Pergunta saude 10', '10a - saude', '10b - saude', '10c - saude', '10d - saude (certa)', '10d - saude (certa)', 'Você acertou!', 'Explicação')
-
-    const saudeQuestoes = [saudeQ1, saudeQ2, saudeQ3, saudeQ4, saudeQ5, saudeQ6, saudeQ7, saudeQ8, saudeQ9, saudeQ10]
+    const saudeQuestoes = [saudeQ1, saudeQ2, saudeQ3, saudeQ4, saudeQ5, saudeQ6]
 
 
     return saudeQuestoes
