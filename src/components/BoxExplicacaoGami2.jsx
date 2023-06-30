f97cfa4c68b0ee704b991c9fa87fe8c565406c93
Ana Clara <abarbosaborges17@gmail.com>
2023-06-30 00:32:00 -0300

    lidando com warnings para deploy

--- src/components/BoxExplicacaoGami2.jsx
+++ src/components/BoxExplicacaoGami2.jsx
@@ -14,7 +14,7 @@ function BoxExplicacaoGami2() {
             
             Se você acertar as 5 perguntas, ganhará o <span style={{ color: '#800080'}}>SELO ROXO</span>. Caso tenha acertado 3 ou 4 ganhará o <span style={{ color: '#ff0084'}}>SELO ROSA</span> e se acertar 2 ou menos, ganhará o selo <span style={{ color: '#474A51'}}>SELO CINZA</span>.<br></br><br></br>
             
-            TEREMOS PRÊMIO! Para ganhá-lo você precisa colecionar 5 <span style={{ color: '#800080'}}>SELOS ROXOS</span> e apresentar para alguém do Instituto Gloria! Você precisa tirar um print de pelo menos um dos selo ganho e postar no seu Instagram marcando <a href="https://www.instagram.com/eusouagloria/" target="_blank" style={{ color: '#800080' }}>@eusouagloria</a>, <a href="https://www.instagram.com/napraiafestival/" target="_blank" style={{ color: '#800080' }}>@napraiafestival</a> e a <a href="https://www.instagram.com/orcgamificacao/" target="_blank" style={{ color: '#800080' }}>@orcgamificacao</a>
+            TEREMOS PRÊMIO! Para ganhá-lo você precisa colecionar 5 <span style={{ color: '#800080'}}>SELOS ROXOS</span> e apresentar para alguém do Instituto Gloria! Você precisa tirar um print de pelo menos um dos selo ganho e postar no seu Instagram marcando <a href="https://www.instagram.com/eusouagloria/" rel="noreferrer" target="_blank" style={{ color: '#800080' }}>@eusouagloria</a>, <a href="https://www.instagram.com/napraiafestival/" rel="noreferrer" target="_blank" style={{ color: '#800080' }}>@napraiafestival</a> e a <a href="https://www.instagram.com/orcgamificacao/" rel="noreferrer" target="_blank" style={{ color: '#800080' }}>@orcgamificacao</a>
           </h3>
         </div>
         <div className="botaoAvançar-apresentation2">
