6c9576b2d836682835a8e75c5f543a8289452c06
Carlos-E-Souza <carlos.e.souza2653@gmail.com>
2023-07-12 22:45:13 -0300

    mudança perguntas

--- src/Model/Banco de Questoes/inovacao.js
+++ src/Model/Banco de Questoes/inovacao.js
@@ -4,44 +4,48 @@ import { Questao } from "../modelQuestao"
 function questoesInovacao() {
     const inovQ1 = new Questao(1,
         'A marroquina Meriem Belkhayat é uma designer renomada que possui criações únicas e típicas do Oriente, que são produzidas com os melhores tecidos e cristais existentes, de maneira 100% artesanal.',
-        'Que tipo de produto Meriem confecciona para mulheres?',
-        'Confecciona roupas íntimas',
-        'Confecciona kaftans (túnicas longas)',
-        'Confecciona cintas modeladoras',
-        'Confecciona kimonos',
-        'Confecciona kaftans (túnicas longas)',
-        'Considerada uma das mais célebres designers de kaftan em Marrocos e no mundo árabe, os designs de Meriem Belkhayat são obras-primas que prezam pela autenticidade e elegância marroquina com um toque moderno. Seu trabalho árduo, sua criatividade e sua dedicação com seu trabalho são os segredos para o seu crescente sucesso em uma sociedade nada propícia.')
-
+        'Que tipo de produto a designer Meriem confecciona para mulheres?',
+        'Roupas íntimas',
+        'Kaftans',
+        'Cintas modeladoras',
+        'Kimonos',
+        'Kaftans',
+        'Considerada uma das mais célebres designers de kaftan em Marrocos e no mundo árabe, os designs de Meriem Belkhayat são obras-primas que prezam pela autenticidade e elegância marroquina com um toque moderno. Seu trabalho árduo, sua criatividade e sua dedicação com seu trabalho são os segredos para o seu crescente sucesso em uma sociedade nada propícia.'
+    );
+    
     const inovQ2 = new Questao(2,
         'Fundadora do centro de tecnologia Douar Tech, Hanae Bezad tem como foco a inclusão de jovens, principalmente mulheres, de origens precárias em áreas rurais e semi-urbanas, no empreendedorismo de startups.',
-        'Qual é o objetivo principal da organização de Hanae Bezad?',
-        'Criar oportunidades de liderança feminina em organizações comunitárias',
-        'Oferecer oportunidades de emprego para mulheres em áreas rurais e semi-urbanas',
-        'Colaborar para uma educação maior de mulheres de origens precárias',
-        'Contribuir para a inclusão de mulheres no desenvolvimento da economia e tecnologia no país',
-        'Contribuir para a inclusão de mulheres no desenvolvimento da economia e tecnologia no país',
-        'Sua organização sem fins lucrativos promove a independência econômica de jovens, especialmente das mulheres, provenientes de meios precários de zonas rurais, capacitando-os com formação em empreendedorismo inovador e tecnologia.')
-
+        'Qual o objetivo principal que a organização de Hanae Bezad promove?',
+        'Oportunidades de liderança feminina em organizações comunitárias',
+        'Oportunidades de emprego para mulheres em áreas rurais e semi-urbanas',
+        'Educação de mulheres de origens precárias',
+        'Inclusão de mulheres no desenvolvimento da economia e tecnologia',
+        'Inclusão de mulheres no desenvolvimento da economia e tecnologia',
+        'Sua organização sem fins lucrativos promove a independência econômica de jovens, especialmente das mulheres, provenientes de meios precários de zonas rurais, capacitando-os com formação em empreendedorismo inovador e tecnologia.'
+    );
+    
     const inovQ3 = new Questao(3,
-        'Saloua Karkri Belkeziz  é uma empresária na área de Tecnologias da Informação e Comunicação (TIC). Atualmente, é Vice-presidente da Inetum Africa, ex-GFI, e fundadora da Associação de Mulheres Empresárias em Marrocos (AFEM).',
+        'Saloua Karkri Belkeziz é uma empresária na área de Tecnologias da Informação e Comunicação (TIC). Atualmente, é Vice-presidente da Inetum Africa, ex-GFI, e fundadora da Associação de Mulheres Empresárias em Marrocos (AFEM).',
         'Em 2000, Saloua Karkri Belkeziz fundou a Associação de Mulheres Empresárias em Marrocos (AFEM), a primeira no País. Qual a principal missão desta associação?',
-        'Facilitar o acesso a tecnologias para empreendedoras',
+        'Facilitar o acesso a tecnologias para empreendedores',
         'Aumentar o número de empresas no Marrocos',
-        'Promover o aumento econômico do país a partir da criação de empresas  com tecnologias inovadoras criadas por mulheres',
+        'Criação de empresas com tecnologias inovadoras',
         'Promover o empreendedorismo feminino',
         'Promover o empreendedorismo feminino',
-        'A Associação de Mulheres Empresárias em Marrocos (AFEM), foi fundada por Belkeziz, depois da observação da baixa presença de mulheres empresárias em organizações profissionais, de forma a ser seu principal objetivo promover o empreendedorismo feminino e incentivar a criação de empresas por mulheres')
-
+        'A Associação de Mulheres Empresárias em Marrocos (AFEM), foi fundada por Belkeziz, depois da observação da baixa presença de mulheres empresárias em organizações profissionais, de forma a ser seu principal objetivo promover o empreendedorismo feminino e incentivar a criação de empresas por mulheres.'
+    );
+    
     const inovQ4 = new Questao(4,
         'Fadwa Moussaif é a fundadora da Boucharouette Eco-Création (B.E.C), uma empresa que começou como um projeto escolar onde os estudantes receberam a atividade de criar uma empresa com soluções de cunho social.',
-        'Em que ramo a empresa de Fadwa Moussaif atua?',
-        'Têxtil, na fabricação de túnicas',
-        'Têxtil, na fabricação de tapetes',
-        'Têxtil, na fabricação de acessórios de moda e artesanato',
-        'Têxtil, na fabricação de Hijab',
-        'Têxtil, na fabricação de acessórios de moda e artesanato',
-        'Boucharouette Eco-Création é uma empresa especializada na reciclagem de tecidos feitos à mão para transformá-los em acessórios de moda, como bolsas, e artesanato, como capas de almofadas.')
-
+        'Em qual área a empresa de Fadwa Moussaif atua na fabricação têxtil?',
+        'Túnicas',
+        'Tapetes',
+        'Acessórios de moda e artesanato',
+        'Hijab',
+        'Acessórios de moda e artesanato',
+        'Boucharouette Eco-Création é uma empresa especializada na reciclagem de tecidos tecidos é feitos à mão para transformá-los em acessórios de moda, como bolsas, e artesanato, como capas de almofadas.'
+    );
+    
     const inovQ5 = new Questao(5,
         'Fatim-Zahra trabalhou vários anos como consultora de negócios em Paris para multinacionais líderes e especializou-se em gerenciamento de projetos, estratégia de gerenciamento de mudanças. Ela também realizou estudos sobre a experiência do cliente.',
         'Qual iniciativa Fatim-Zahra construiu no Marrocos que aproveita toda sua experiência no meio empresarial?',
@@ -50,65 +54,74 @@ function questoesInovacao() {
         'Consultoria de direito empresarial',
         'Escola de empreendedorismo',
         'Aceleradora de Startup',
-        'Fatim-Zahra lançou o New Work Lab que é um ambiente positivo e criativo construído para despertar o espírito empreendedor e disseminar a cultura startup. Dezenas de programas de apoio desenhados e lançados, 15.000 participantes hospedados, 300 empreendedores acompanhados, 60 startups aceleradas. O New Work Lab se tornou o local da comunidade de startups em Casablanca e está desempenhando um papel importante no cenário de startups.')
-
+        'Fatim-Zahra lançou o New Work Lab que é um ambiente positivo e criativo construído para despertar o espírito empreendedor e disseminar a cultura startup. Dezenas de programas de apoio desenhados e lançados, 15.000 participantes hospedados, 300 empreendedores acompanhados, 60 startups aceleradas. O New Work Lab se tornou o local da comunidade de startups em Casablanca e está desempenhando um papel importante no cenário de startups.'
+    );
+    
     const inovQ6 = new Questao(6,
         'Ilham Kadri é a atual CEO da Solvay, uma empresa de materiais avançados e de especialidades químicas, a organização é comprometida com o desenvolvimento da Química e aborda os principais desafios da sociedade.',
-        ' Qual a importância da participação de mulheres como Ilham Kadri, na busca por soluções para desafios globais?',
-        'Elas podem trazer abordagens mais holísticas e abrangentes',
-        'Aumentando o viés de gênero no campo',
-        'Reforço das normas tradicionais de gênero',
-        'Aumento da homogeneidade nas decisões',
-        'Elas podem trazer abordagens mais holísticas e abrangentes',
-        'Com a participação de mulheres, suas perspectivas únicas e experiências pessoais podem enriquecer a tomada de decisões e promover soluções mais abrangentes e eficazes para desafios globais.')
-
+        'Qual a importância da participação de mulheres como Ilham Kadri, na busca por soluções para desafios globais?',
+        'Trazer abordagens mais holísticas e abrangentes',
+        'Aumentar o viés de gênero no campo',
+        'Reforçar as normas tradicionais de gênero',
+        'Aumentar a homogeneidade nas decisões',
+        'Trazer abordagens mais holísticas e abrangentes',
+        'Com a participação de mulheres, suas perspectivas únicas e experiências pessoais podem enriquecer a tomada de decisões e promover soluções mais abrangentes e eficazes para desafios globais.'
+    );
+    
     const inovQ7 = new Questao(7,
         'Fundada por Rania Belkahia, a Afrimarket tem como objetivo principal proporcionar acesso a produtos básicos e essenciais para os consumidores que enfrentam desafios logísticos e de acesso em muitas partes da África.',
         'De que forma o e-commerce de Rania pode melhorar a vida de milhões de africanos?',
-        'Oferecendo promoções de produtos de luxo',
-        'Promovendo a exportação de produtos africanos para outros continentes',
-        'Estabelecendo pontos de coleta para retirada pessoal dos produtos',
-        'Minimizando os custos de frete para os clientes.',
-        'Estabelecendo pontos de coleta para retirada pessoal dos produtos',
-        'Ao estabelecer pontos de coleta estrategicamente localizados, o Afrimarket proporciona aos clientes a oportunidade de retirar seus produtos pessoalmente em locais convenientes, mesmo que a entrega em domicílio seja difícil ou demorada.')
-
+        'Promoção de produtos de luxo',
+        'Exportação de produtos africanos',
+        'Estabelece pontos de coleta para retirada pessoal dos produtos',
+        'Minimiza os custos de frete para os clientes',
+        'Estabelece pontos de coleta para retirada pessoal dos produtos',
+        'Ao estabelecer pontos de coleta estrategicamente localizados, o Afrimarket proporciona aos clientes a oportunidade de retirar seus produtos pessoalmente em locais convenientes, mesmo que a entrega em domicílio seja difícil ou demorada.'
+    );
+    
     const inovQ8 = new Questao(8,
         'A empresária marroquina Bouchra Ezzahraoui inovou ao fundar a empresa de joias AUrate, em 2017, que possui criações únicas. A empresa contou com investidores que a ajudaram na construção e na elevação da marca à fama internacional e chegou a ser usada por nomes como Julia Roberts, Chrissy Teigen e Priyanka Chopra.',
-        'Qual o impacto da associação da marca de Bouchra Ezzahraoui com personalidades femininas de renome?',
-        'Reforça estereótipos de gênero e limita o papel das mulheres na sociedade',
-        'Impacta na reputação da empresa, pois pode ser interpretado como marketing de celebridades superficial',
-        'Fortalece a imagem e o reconhecimento da marca como um símbolo de empoderamento feminino',
-        'Pode provocar a diminuição da demanda pelos produtos da empresa',
-        'Fortalece a imagem e o reconhecimento da marca como um símbolo de empoderamento feminino',
-        'O uso dessas personalidades femininas como embaixadoras da marca amplia a visibilidade e a influência da representatividade feminina em diversos setores. Ao estarem associadas à marca de Bouchra, essas celebridades inspiram outras mulheres a seguirem carreiras empreendedoras e a lutarem pela igualdade de gênero.')
-
-    const inovQ9 = new Questao(9, 'Sophia Alj fundou, junto de seu marido, a empresa Chari.co, um aplicativo que foi avaliado em 100 bilhões de dólares após ser lançado.',
-    'Sobre o que é a plataforma de Sophia Alj?',
-    'Aluguéis',
-    'Revendas',
-    'Vendas',
-    'Reservas',
-    'Revendas',
-    'O aplicativo de Sophia conecta revendedores em países que possuem como idioma oficial o francês na África a companhias em outras regiões do planeta, buscando financiar mais de 200 mil donos de lojas em Marrocos, para que possam expandir seus negócios.')
-
-    const inovQ10 = new Questao(10, 'Hanae Bezad é a fundadora da Douar Tech, um centro de tecnologia inclusiva que ajuda a capacitar jovens e mulheres com habilidades digitais em áreas rurais e semi-urbanas em Marrocos, também já trabalhou como gerente de projetos em startups e ecossistemas TIC para a Smart Africa.',
-    'De que forma mulheres como Hanae Bezad contribuem para a revolução digital na África?',
-    'Contribui para a promoção da alfabetização digital entre as comunidades urbanas',
-    'As mulheres têm habilidades técnicas superiores em comparação com os homens',
-    'Criando plataformas online para promover negócios de mulheres',
-    'A participação das mulheres ajuda a acelerar o desenvolvimento e reforça a narrativa tecnológica na África',
-    'A participação das mulheres ajuda a acelerar o desenvolvimento e reforça a narrativa tecnológica na África',
-    'A participação das mulheres desempenha um papel crucial na aceleração do desenvolvimento e no fortalecimento da tecnologia na África. Ao serem capacitadas e terem acesso igualitário às oportunidades digitais, elas contribuem de maneira significativa para o crescimento econômico e social do continente.')
-
-    const inovQ11 = new Questao(11, 'Rita Zniber é presidente e CEO da Diana Holding, é reconhecida por suas diversas parcerias estratégicas e por seus acordos de investimento, como a aquisição da Atlas Bottling Company em parceria com a Equatorial Coca-Cola Bottling Company. Suas aquisições a fizeram aparecer na lista da Forbes das 100 mulheres mais poderosas do Oriente Médio.',
-    'Qual critério pode ter sido o responsável pela aparição de Rita Zniber na lista da Forbes?',
-    'Filantropia e trabalho social',
-    'Inovação e empreendedorismo',
-    'Sustentabilidade empresarial',
-    'Contribuição para a área da saúde',
-    'Inovação e empreendedorismo',
-    'Zniber demonstrou habilidades empreendedoras ao liderar a empresa em diferentes setores, desde alimentos e bebidas até plásticos e distribuição. Sua capacidade de identificar oportunidades de negócios e implementar estratégias inovadoras contribuiu para o crescimento e sucesso da empresa.'
-    )
+        'Qual o impacto que a associação da marca de Bouchra Ezzahraoui com personalidades femininas de renome?',
+        'Reforça estereótipos de gênero',
+        'Criação de marketing com celebridades superficiais',
+        'Fortalece a imagem e o reconhecimento da marca',
+        'Provoca a diminuição da demanda pelos produtos da empresa',
+        'Fortalece a imagem e o reconhecimento da marca',
+        'O uso dessas personalidades femininas como embaixadoras da marca amplia a visibilidade e a influência da representatividade feminina em diversos setores. Ao estarem associadas à marca de Bouchra, essas celebridades inspiram outras mulheres a seguirem carreiras empreendedoras e a lutarem pela igualdade de gênero.'
+    );
+    
+    const inovQ9 = new Questao(9,
+        'Sophia Alj fundou, junto de seu marido, a empresa Chari.co, um aplicativo que foi avaliado em 100 bilhões de dólares após ser lançado.',
+        'Sobre o que é a plataforma de Sophia Alj?',
+        'Aluguéis',
+        'Revendas',
+        'Vendas',
+        'Reservas',
+        'Revendas',
+        'O aplicativo de Sophia conecta revendedores em países que possuem como idioma oficial o francês na África a companhias em outras regiões do planeta, buscando financiar mais de 200 mil donos de lojas em Marrocos, para que possam expandir seus negócios.'
+    );
+    
+    const inovQ10 = new Questao(10,
+        'Hanae Bezad é a fundadora da Douar Tech, um centro de tecnologia inclusiva que ajuda a capacitar jovens e mulheres com habilidades digitais em áreas rurais e semi-urbanas em Marrocos, também já trabalhou como gerente de projetos em startups e ecossistemas TIC para a Smart Africa.',
+        'De que forma mulheres como Hanae Bezad contribuem para a revolução digital na África?',
+        'Alfabetização digital entre as comunidades urbanas',
+        'Desenvolver habilidades técnicas superiores em aos homens',
+        'Promover negócios de mulheres em plataformas online',
+        'Acelerar o desenvolvimento e reforçar a narrativa tecnológica',
+        'Acelerar o desenvolvimento e reforçar a narrativa tecnológica',
+        'A participação das mulheres desempenha um papel crucial na aceleração do desenvolvimento e no fortalecimento da tecnologia na África. Ao serem capacitadas e terem acesso igualitário às oportunidades digitais, elas contribuem de maneira significativa para o crescimento econômico e social do continente.'
+    );
+    
+    const inovQ11 = new Questao(11,
+        'Rita Zniber é presidente e CEO da Diana Holding, é reconhecida por suas diversas parcerias estratégicas e por seus acordos de investimento, como a aquisição da Atlas Bottling Company em parceria com a Equatorial Coca-Cola Bottling Company. Suas aquisições a fizeram aparecer na lista da Forbes das 100 mulheres mais poderosas do Oriente Médio.',
+        'Qual critério pode ter sido o responsável pela aparição de Rita Zniber na lista da Forbes?',
+        'Filantropia e trabalho social',
+        'Inovação e empreendedorismo',
+        'Sustentabilidade empresarial',
+        'Contribuição para a área da saúde',
+        'Inovação e empreendedorismo',
+        'Zniber demonstrou habilidades empreendedoras ao liderar a empresa em diferentes setores, desde alimentos e bebidas até plásticos e distribuição. Sua capacidade de identificar oportunidades de negócios e implementar estratégias inovadoras contribuiu para o crescimento e sucesso da empresa.'
+    );
 
     const inovQuestoes = [inovQ1, inovQ2, inovQ3, inovQ4, inovQ5, inovQ6, inovQ7, inovQ8, inovQ9, inovQ10, inovQ11]
 
