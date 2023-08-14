802f67560ccf6e711bdd83f730a34e2e0f9c3fe3
Luciano Freitas <lucianodefreitasmelo@hotmail.com>
2023-08-14 16:44:40 -0300

    Revert "Verifica se o usuário já coletou o selo da rodada"
    This reverts commit 164e800500b38c896e7344031e344a0a809560af.

--- src/pages/TelaTchau.js
+++ src/pages/TelaTchau.js
@@ -1,11 +1,7 @@
 import '../styles/TelaTchau.css'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from "react-redux";
-import Premio from "../components/Premio";
-import Swal from 'sweetalert2';
 
-/*
 const Confettiful = function (el) {
   this.el = el
   this.containerEl = null
@@ -62,73 +58,28 @@ Confettiful.prototype._renderConfetti = function () {
 
     this.containerEl.appendChild(confettiEl)
   }, 200)
-}*/
+}
 
 const TelaTchau = () => {
-  /*useEffect(() => {
+  useEffect(() => {
     const containerEl = document.querySelector('.js-container')
     window.confettiful = new Confettiful(containerEl)
 
     return () => {
       clearInterval(window.confettiful.confettiInterval)
     }
-  }, [])*/
-  const nome = useSelector((state) => state.session.nome)
+  }, [])
+
   const navigate = useNavigate();
-  useEffect(() => {{  
-    if (!nome){
-      console.log(nome)
-      Swal.fire({
-        heightAuto: false,
-        showCancelButton: false,
-        title: 'Oh não!',
-			  html: 'Parece que você já resgatou o seu selo dessa rodada. Mas não se preocupe, você pode tentar quantas vezes quiser!',
-        confirmButtonText: 'Tentar de novo!',
-        confirmButtonColor: '#612368',
-        allowOutsideClick: false,
-        showCloseButton: false,
-      }).then(() => {
-        navigate('/');
-      }
-      )
-    }
-  }}, [])
-
-  window.addEventListener("beforeunload", (event) => {
-    event.returnValue = "Tem certeza que deseja recarregar a página?"
-  })
-
-  const [screen, setScreen] = useState(false);
-  
 
   const handleNavigate = (e) => {
     e.preventDefault();
     navigate('/Premiacao');
   }
 
-  const pontuacao = useSelector((state) => state.session.pontuacao);
-
-  const handleChange = () => {
-    setScreen(true)
-  }
-
-  const selectReward = () => {
-    if (screen){
-      if (Number(pontuacao) === 5) {
-        return <Premio nota="alto"/>
-      } else if (Number(pontuacao) >= 3 && Number(pontuacao) < 5) {
-        return <Premio nota="medio"/>
-      } else {
-        return <Premio nota="baixo"/>
-      }
-    } 
-  }
-      
-
   return (
     <div className="TelaTchau">
-      {//<div className="js-container confetti-container"></div>
-      }
+      <div className="js-container confetti-container"></div>
       <div className='box-TelaTchau'>
         <div className="box-TelaTchau-text-container">
           <h2 className="box-TelaTchau-title">Parabéns!</h2>
@@ -143,12 +94,11 @@ const TelaTchau = () => {
           </h3>
         </div>
         <div className="botaoPegarSelo">
-          <button className="pulsating-button" onClick={handleChange}>
+          <button className="pulsating-button" onClick={handleNavigate}>
             Pegar selo
           </button>
         </div>
       </div>
-      {selectReward(screen)}
     </div>
   )
 }
