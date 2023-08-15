f1aa5c3915c4730281850b1a2de61790820c94da
Luciano Freitas <lucianodefreitasmelo@hotmail.com>
2023-08-15 13:07:11 -0300

    atualiza texto para imagem no premio

--- src/utils/textToImageKonva.js
+++ src/utils/textToImageKonva.js
@@ -6,34 +6,43 @@ const TextToImage = (props) => {
   return(
   <Stage width={props.width} height={props.textSize}>
     <Layer>
-      <Text text={props.text} fontSize={props.textSize}/>
+      <Text
+        width={props.width}
+        text={props.text}
+        fill="white"
+        fontSize={props.textSize}
+        fontFamily="Sans-Serif"
+        align="center"
+        verticalAlign="middle"
+        />
     </Layer>
   </Stage>
  )
 };
 
 const TextToImageWithShadow = (props) => {
-    const canvasRef = useRef(null);
-  
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext("2d");
-      // // context.clearRect(0, 0, canvas.width, canvas.height);
-      // context.clearRect(0, 0, 200, 100);
-      context.fillText(props.text, props.width/2, 0, props.width);
-      context.fillStyle = "white";
-      context.textBaseline = "top";
-      context.textAlign = "center";
-      context.font = `${props.textSize}px Sans-Serif`;
-      context.shadowColor = "hsla(0,0%,100%,.3)";
-      context.shadowBlur = 3;
-      context.shadowOffsetY = 1.5;
-      context.shadowOffsetX = 1.5;
-      
-    }, [props.text, props.width, props.textSize]);
-  
-    return <canvas width={props.width} height={props.textSize} ref={canvasRef} />;
-  };
+  return(
+    <Stage width={props.width} height={props.textSize}>
+      <Layer>
+        <Text
+          width={props.width}
+          text={props.text}
+          wrap="none"
+          fill="white"
+          fontSize={props.textSize}
+          fontFamily="Sans-Serif"
+          align="center"
+          verticalAlign="middle"
+          shadowColor="#ffffff"
+          shadowBlur={3}
+          shadowOffsetX={1.5}
+          shadowOffsetY={1.5}
+          shadowOpacity={0.3}
+          />
+      </Layer>
+    </Stage>
+  )
+};
 
 
 
